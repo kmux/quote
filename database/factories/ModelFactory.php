94f8a57ee2252997f8350b39a3f83cf33d43b940
kmux <nicolaepascaneanu@gmail.com>
2016-03-22 12:03:21 +0200

    Modified model factory for database seed and changed readme file.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,9 @@
 |
 */
 
-$factory->define(Quote\User::class, function (Faker\Generator $faker) {
+$factory->define(Quote\Quote::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'username' => $faker->name,
+        'title' => $faker->sentence(),
     ];
 });
